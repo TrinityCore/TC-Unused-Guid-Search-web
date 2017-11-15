28128534d5e87535db26147816dabe74caa80c59
Aokromes <Aokromes@users.noreply.github.com>
2017-11-15 19:59:57 +0100

    Make all http https

--- index.php
+++ index.php
@@ -284,7 +284,7 @@ $continuous_checked = $continuous ? "checked" : "";
     ?>
     <hr>
     <h4 class="text-center">
-        Coded by <a href="http://www.github.com/ShinDarth">ShinDarth</a>&nbsp;<iframe style="vertical-align: middle;" src="http://ghbtns.com/github-btn.html?user=ShinDarth&repo=TC-Unused-Guid-Search-web&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe>
+        Coded by <a href="https://www.github.com/ShinDarth">ShinDarth</a>&nbsp;<iframe style="vertical-align: middle;" src="https://ghbtns.com/github-btn.html?user=ShinDarth&repo=TC-Unused-Guid-Search-web&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110" height="20"></iframe>
     </h4>
     <a href="https://github.com/TrinityCore/TC-Unused-Guid-Search-web">
         <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67" alt="Get me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png">
